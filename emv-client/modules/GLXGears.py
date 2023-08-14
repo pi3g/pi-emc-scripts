6dc3036ee169f32e0e9c485d13a6c1d704db8acf
Chloe <chloe.pr@pm.me>
2023-08-14 13:02:24 +0200

    update display command

--- emv-client/modules/GLXGears.py
+++ emv-client/modules/GLXGears.py
@@ -1,6 +1,7 @@
 from modules.ModuleBase import ModuleBase
 
-COMMAND="DISPLAY=:0 glxgears -fullscreen >> {0} 2>&1"
+#COMMAND="DISPLAY=:0 glxgears -fullscreen >> {0} 2>&1"
+COMMAND="while true; do cat /dev/urandom > /dev/fb1 2> /dev/null; sleep 0.1; done"
 
 class GLXGears(ModuleBase):
     def __init__(self, logfile: str):
